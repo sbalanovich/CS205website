4a859e9eaf227045bc6c6457f547fe866081b067
Serguei Balanovich <sbalanovuch@college.harvard.edu>
2015-12-09 12:22:12 +0500

    Still can't get local logs to work so keep pushing

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html')
+// app.set('view engine', 'html')
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.sendFile('index.html');
 });
 
 app.listen(app.get('port'), function() {
